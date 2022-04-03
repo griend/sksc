71490b8645cfb7a7c8e3699096ea742962e53dc8
Cees van de Griend <c.vande.griend@gmail.com>
2022-04-03 16:07:40 +0200

    fixed pylint errors

--- src/python/sksc/projectname/__main__.py
+++ src/python/sksc/projectname/__main__.py
@@ -12,39 +12,37 @@ import re
 
 
 def generate() -> str:
-    seed = 'bcdfghjklmnpqrstvwxz'
-    code = ''.join(random.choices(seed, k=4))
-    return code
+    """ Generate a 4 letter code as project name. """
+    return ''.join(random.choices('bcdfghjklmnpqrstvwxz', k=4))
 
 
-def search(code: str) -> str:
-    tmp = f'^{code[0:1]}.*{code[1:2]}.*{code[2:3]}.*{code[3:4]}'
-    return tmp
+def search(tmp: str) -> str:
+    """ Create a REGEX from a 4 letter project code. """
+    return f'^{tmp[0:1]}.*{tmp[1:2]}.*{tmp[2:3]}.*{tmp[3:4]}'
 
 
-def names(search: str):
+def names(tmp: str):
+    """ Search for project names in the words file. """
     words = "/usr/share/dict/words"
-    found = list()
+    found = []
 
-    with open(words) as f:
-        lines = f.readlines()
+    with open(words, encoding='UTF-8') as word_file:
+        lines = word_file.readlines()
 
         for line in lines:
-            if re.findall(search, line):
+            if re.findall(tmp, line):
                 found.append(line.strip())
 
     return found
 
 
 if __name__ == '__main__':
-    code = None
-    regex = None
-    found = None
+    FOUND = None
 
-    while not found:
-        code = generate()
-        regex = search(code)
-        found = names(regex)
+    while not FOUND:
+        CODE = generate()
+        REGEX = search(CODE)
+        FOUND = names(REGEX)
 
-    for name in found:
-        print(f'{code} / {name}')
+    for name in FOUND:
+        print(f'{CODE} / {name}')
